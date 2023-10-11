967227641da09a041c69c0828389897b5fafd33c
Rabbit49 <akasagarbha9@naver.com>
2023-10-11 17:31:36 +0900

    이미지

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Test : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("abcd");
+    }
+}

d5974d02f35f3c1f1c2777c1f36f73266fce8b73
DongSun <duaehdtjs20@gmail.com>
2023-10-23 12:15:16 +0900

    add: note clear effect

--- Assets/Scripts/Rhythm/ClearEffect.cs
+++ Assets/Scripts/Rhythm/ClearEffect.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ClearEffect : MonoBehaviour
 {
     public float Speed = 3f;
+    public GameObject Ring;
     private SpriteRenderer render;
     private Color[] color;
     private Color startColor;
@@ -24,18 +25,28 @@ public class ClearEffect : MonoBehaviour
     }
     public void GetJudge(Judge judge)
     {
+        GameObject ring;
         switch (judge)
         {
             case Judge.PERFECT:
                 endColor = color[0];
+                Ring.GetComponent<SpriteRenderer>().color = color[0];
+                ring = Instantiate(Ring, transform);
+                ring.transform.localPosition = Vector2.zero;
                 timer = 1f;
                 break;
             case Judge.GREAT:
                 endColor = color[1];
+                Ring.GetComponent<SpriteRenderer>().color = color[1];
+                ring = Instantiate(Ring, transform);
+                ring.transform.localPosition = Vector2.zero;
                 timer = 1f;
                 break;
             case Judge.GOOD:
                 endColor = color[2];
+                Ring.GetComponent<SpriteRenderer>().color = color[2];
+                ring = Instantiate(Ring, transform);
+                ring.transform.localPosition = Vector2.zero;
                 timer = 1f;
                 break;
             default:

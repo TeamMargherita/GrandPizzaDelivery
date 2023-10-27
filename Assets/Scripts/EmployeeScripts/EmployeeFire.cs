ab3306b85f8db816f065953bcd2054b7b9bed683
pagi <selly2021392001@gmail.com>
2023-10-27 17:41:12 +0900

    ..

--- Assets/Scripts/EmployeeScripts/EmployeeFire.cs
+++ Assets/Scripts/EmployeeScripts/EmployeeFire.cs
@@ -34,7 +34,6 @@ public class EmployeeFire : MonoBehaviour
         }
     }
 
-    // �׻� �����ְ�, �˹� ���� ��ư �ȴ����� �ٷ� �ǰ� �ٲٱ�!!
     public void ShowFireWin()
     {
         string EmployeeStat = null;

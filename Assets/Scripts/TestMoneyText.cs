74f6ae42154a259e4ebc5a20a01fbfc1b88723fd
inP <dyals713@gmail.com>
2023-10-30 00:21:00 +0900

    머니이펙트 수정중

--- Assets/Scripts/TestMoneyText.cs
+++ Assets/Scripts/TestMoneyText.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class TestMoneyText : MonoBehaviour
 {
+    [SerializeField] private GameObject MoneyEffectPrefab;
+    [SerializeField] private GameObject EffectParent;
     public GameObject money;
 
+    private void Awake()
+    {
+        GameManager.Instance.MoneyText = this;
+    }
+    public void CreateMoneyEffect(int changeMoney)
+    {
+        Debug.Log("머니이펙트생성");
+        int digitPosition = ((int)(Math.Log10(GameManager.Instance.Money) + 1) - (int)(Math.Log10(changeMoney) + 1)) * 28;
+        EffectParent.GetComponent<RectTransform>().anchoredPosition = new Vector2(digitPosition, -50);
+        GameObject moneyEffect = Instantiate(MoneyEffectPrefab, transform.position, transform.rotation, EffectParent.transform);
+        moneyEffect.GetComponent<Text>().text = Convert.ToString(changeMoney);
+    }
 
     void Update()
     {

f6f0bb54f34f2a912bcf3a0d5e952ffa0d3e7490
pagi <selly2021392001@gmail.com>
2023-11-01 17:18:26 +0900

    UI사이즈 변경

--- Assets/Scripts/EmployeeScripts/EmployeeRecruit.cs
+++ Assets/Scripts/EmployeeScripts/EmployeeRecruit.cs
@@ -33,6 +33,8 @@ public class EmployeeRecruit : MonoBehaviour
         {
             preferedDay.Add(i, new List<Day>());
         }
+
+        isMorning = true;
     }
 
     private void Update()

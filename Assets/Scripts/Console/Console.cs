1d9ddfd22f762c0ec93b80270b4897eeb19342d3
ino <111103274+samgirlsss@users.noreply.github.com>
2023-11-06 14:50:24 +0900

    콘솔 치트키 창 만듬 패널에 이제 텍스트 띄워야지

--- Assets/Scripts/Console/Console.cs
+++ Assets/Scripts/Console/Console.cs
@@ -6,8 +6,19 @@ using UnityEngine.UI;
 public class Console : MonoBehaviour
 {
 	public InputField mainInputField;
+    public GameObject ExplainPanel;
+
+    private string[] Key = new string[] {"Player.", "Time."};
+    private List<string> player = new List<string>() {"AddMoney", "Dead", "God" };
+    private List<string> time = new List<string>() { "Stop", "Play", "NextDay", "DarkDay" };
+    private Dictionary<string, List<string>> ConsolDB = new Dictionary<string, List<string>>();
 
     private bool setActive = false;
+    private void Awake()
+    {
+        ConsolDB.Add(Key[0], player);
+        ConsolDB.Add(Key[1], time);
+    }
     void InputConsole()
     {
         if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
@@ -56,6 +67,56 @@ public class Console : MonoBehaviour
             }
 		}
 	}
+    int index = 1;
+    string firstString = "";
+    public void AutoText()
+    {
+        if (firstString == mainInputField.text) { index = 1; }
+        string key = "";
+        if(index == 1)
+        {
+            foreach (var i in Key)
+            {
+                if (i.Contains(mainInputField.text))
+                {
+                    key = i;
+                    break;
+                }
+            }
+            //자동완성텍스트 업데이트
+            if (mainInputField.text.Length > 0)
+                ExplainPanel.transform.GetChild(0).GetComponent<Text>().text = key;
+            else
+            {
+                ExplainPanel.transform.GetChild(0).GetComponent<Text>().text = "";
+                index = 1;
+            }
+            //ConsolDB딕셔너리에서 키값이 인풋필드 텍스트와 일치해지면 콘솔치트의 다음블록으로 넘어가짐 백스페이스로 지우지않을때
+            if (ConsolDB.ContainsKey(mainInputField.text) && !Input.GetKeyDown(KeyCode.Backspace))
+            {
+                index = 2;
+                firstString = mainInputField.text;
+            }
+        }
+        else if(index == 2)
+        {
+            for (int i = 0; i < ConsolDB[firstString].Count; i++)
+            {
+                if (ConsolDB[firstString][i].Contains(mainInputField.text.Substring(firstString.Length)))
+                {
+                    key = ConsolDB[firstString][i];
+                    break;
+                }
+            }
+            if (mainInputField.text.Length > 0)
+                ExplainPanel.transform.GetChild(0).GetComponent<Text>().text = firstString + key;
+            else
+            {
+                ExplainPanel.transform.GetChild(0).GetComponent<Text>().text = "";
+                index = 1;
+            }
+        }
+    }
 
     private void Update()
     {
@@ -69,6 +130,8 @@ public class Console : MonoBehaviour
             transform.GetChild(0).gameObject.SetActive(false);
             setActive = false;
             mainInputField.text = "";
+            firstString = "";
+            index = 1;
         }
         InputConsole();
 	}

7874e6cdb6fd0ee2f0eda6292e8849d04ee6ca74
rabbit <akasagarbha9@naver.com>
2023-11-14 00:00:24 +0900

    짜

--- Assets/Scripts/UIs/PlayerTexts.cs
+++ Assets/Scripts/UIs/PlayerTexts.cs
@@ -6,9 +6,12 @@ using UnityEngine.EventSystems;
 
 // 한석호 작성
 
-public class PlayerTexts : MonoBehaviour, IPointerClickHandler
+public class PlayerTexts : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Image panelImg;
 
+    private static Color oneC = new Color(231 / 255f, 102 / 255f, 102 / 255f, 100 / 255f);
+    private static Color twoC = new Color(255 / 255f, 255 / 255f, 255 / 255f, 100 / 255f);
     public int TextNum { get; set; }
 
     private IInspectingUIText iInspectingUIText;
@@ -23,4 +26,14 @@ public class PlayerTexts : MonoBehaviour, IPointerClickHandler
         Debug.Log(TextNum);
         iInspectingUIText.ChoiceText(TextNum);
     }
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+        panelImg.color = oneC;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+        panelImg.color = twoC;
+	}
 }
